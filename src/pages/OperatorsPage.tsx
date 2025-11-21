f5bcebb3581f9c9c9d15aa27944cec75901eb4ad
Mariusz Roliński <mariusz@mondoreale.pl>
2025-11-21 15:33:46 +0100

    fix [FRONT-2358]: Truncate operator *names* with CSS (#2004)
    * Truncate operator *names* with CSS
    
    * Apply the same logic to `DelegationsTable` for consistency

--- src/pages/OperatorsPage.tsx
+++ src/pages/OperatorsPage.tsx
@@ -244,6 +244,7 @@ function DelegationsTable({
                             operatorId={id}
                             operatorName={name}
                             imageUrl={imageUrl}
+                            truncate={!!name}
                         />
                     ),
                     align: 'start',
@@ -331,6 +332,7 @@ function OperatorsTable({
                             operatorId={id}
                             operatorName={name}
                             imageUrl={imageUrl}
+                            truncate={!!name}
                         />
                     ),
                     align: 'start',
